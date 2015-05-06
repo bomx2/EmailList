77736cb722e8c78cd9b5dc02647b45ecb05e4aa3
김유정 <saramary21@gmail.com>
2015-05-06 17:43:05 +0900

    first commit!

---
+++ src/com/sds/icto/emaillist/dao/EmailListDAO.java
@@ -0,0 +1,18 @@
+package com.sds.icto.emaillist.dao;
+
+import java.util.List;
+
+import com.sds.icto.emaillist.vo.EmailListVO;
+
+public class EmailListDAO {
+	
+	public void insert(){
+		
+	}
+	
+	public List<EmailListVO> fetchList(){
+		
+		return null;
+	}
+
+}
